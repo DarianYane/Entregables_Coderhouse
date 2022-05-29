f62dc8bac708e43ddfdb4b4b4b09ede396100908
Darian Yane <103910872+DarianYane@users.noreply.github.com>
2022-05-29 09:44:51 -0300

    Update Funciones  - Yane Darian.py

--- Funciones  - Yane Darian.py
+++ Funciones  - Yane Darian.py
@@ -1,4 +1,9 @@
 #1
+"""
+1) Realiza una función llamada area_rectangulo() que devuelva el área del rectángulo a partir de una base y una altura. Calcula el área de un rectángulo de 15 de base y 10 de altura
+
+🖐 Ayuda: El área de un rectángulo se obtiene al multiplicar la base por la altura.
+"""
 
 #Defino la función
 def area_rectangulo(base, altura):
@@ -16,6 +21,11 @@ area=area_rectangulo(base, altura)
 print(f"El área de un rectángulo de base {base} y altura {altura} es {area}")
 
 #2
+"""
+2) Realiza una función llamada area_circulo() que devuelva el área de un círculo a partir de un radio. Calcula el área de un círculo de 5 de radio
+
+🖐 Ayuda: El área de un círculo se obtiene al elevar el radio a dos y multiplicando el resultado por el número pi. Puedes utilizar el valor 3.14159 como pi o importarlo del módulo math.
+"""
 
 # Importo pi
 import math
@@ -40,6 +50,13 @@ print(f"El área de un círculo de radio {radio} es {area}")
 
 
 #3
+"""
+3) Realiza una función llamada relacion() que a partir de dos números cumpla lo siguiente:
+
+Si el primer número es mayor que el segundo, debe devolver 1. Si el primer número es menor que el segundo, debe devolver -1. Si ambos números son iguales, debe devolver un 0.
+
+Comprueba la relación entre los números: '5 y 10', '10 y 5' y '5 y 5'
+"""
 
 #Defino la función
 def relacion(n1, n2):
@@ -60,6 +77,13 @@ relacion(n1, n2)
 
 
 #4
+"""
+4) Realiza una función llamada intermedio() que a partir de dos números, devuelva su punto intermedio:
+
+🖐 Ayuda: El número intermedio de dos números corresponde a la suma de los dos números dividida entre 2
+
+Comprueba el punto intermedio entre -12 y 24
+"""
 
 #Defino la función
 def intermedio(n1, n2):
@@ -78,6 +102,18 @@ print(f"El punto intermedio entre {n1} y {n2} es {punto}.")
 
 
 #5
+"""
+5) Realizá una función llamada recortar() que reciba tres parámetros. El primero es el número a recortar, el segundo es el límite inferior y el tercero el límite superior. La función tendrá que cumplir lo siguiente:
+
+Devolver el límite inferior si el número es menor que éste
+
+Devolver el límite superior si el número es mayor que éste.
+
+Devolver el número sin cambios si no se supera ningún límite.
+
+Comprueba el resultado de recortar 15 entre los límites 0 y 10
+"""
+
 
 #Defino la función
 def recortar(rec, inf, sup):
@@ -102,6 +138,11 @@ recortar(rec, inf, sup)
 
 
 #6
+"""
+6) Realiza una función separar() que tome una lista de números enteros y devuelva dos listas ordenadas. La primera con los números pares, y la segunda con los números impares:
+
+🖐 Ayuda: Para ordenar una lista automáticamente puedes usar el método .sort()
+"""
 
 # Defino la función separar()
 def separar(original):
